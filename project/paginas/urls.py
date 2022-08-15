ace98c715d44fd0539bfa0191544a8260bc01e5d
lucasedbraga <braga.lucas@engenharia.ufjf.br>
2022-08-15 15:41:28 -0300

    aprendendo_importar_template

--- project/paginas/urls.py
+++ project/paginas/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import PaginaInicial
+from .views import PaginaInicial, SobreView
 
 urlpatterns = [
     #path(endereco, qual_sera_a_view, nome_para_url)
-    path('inicio/', PaginaInicial.as_view(), name='inicio')
+    path('inicio/', PaginaInicial.as_view(), name='inicio'),
+    path('sobre/',SobreView.as_view(), name='sobre')
+
 ]
--- project/paginas/views.py
+++ project/paginas/views.py
@@ -3,4 +3,7 @@ from django.views.generic import TemplateView
 # Create your views here.
 class PaginaInicial(TemplateView):
     # Classe para página inicial
-    template_name= "paginas/index.html"
+    template_name= "paginas/modelo.html"
+
+class SobreView(TemplateView):
+    template_name= 'paginas/sobre.html'
