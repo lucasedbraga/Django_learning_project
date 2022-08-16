16819d39657d8191eee7319e0522ab91d2f7a59a
lucasedbraga <braga.lucas@engenharia.ufjf.br>
2022-08-16 15:44:02 -0300

    aula08: alterando e deletando registros

--- project/cadastros/urls.py
+++ project/cadastros/urls.py
@@ -1,7 +1,15 @@
 from django.urls import path
 from .views import CampoCreate, AtividadeCreate
+from .views import CampoUpdate, AtividadeUpdate
+from .views import CampoDelete, AtividadeDelete
 
 urlpatterns = [
     path('cadastrar/campo/', CampoCreate.as_view(), name='cadastrar-campo'),
     path('cadastrar/atividade/', AtividadeCreate.as_view(), name='cadastrar-atividade'),
+    
+    path('editar/campo/<int:pk>/', CampoUpdate.as_view(), name='editar-campo'),
+    path('editar/atividade/<int:pk>/', AtividadeUpdate.as_view(), name='editar-atividade'),
+
+    path('excluir/campo/<int:pk>/', CampoDelete.as_view(), name='excluir-campo'),
+    path('excluir/atividade/<int:pk>/', AtividadeDelete.as_view(), name='excluir-atividade'),
 ]
--- project/cadastros/views.py
+++ project/cadastros/views.py
@@ -1,4 +1,4 @@
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Campo, Atividade
 from django.urls import reverse_lazy
 
@@ -14,3 +14,28 @@ class AtividadeCreate(CreateView):
     fields = ['numero', 'descricao', 'pontos', 'detalhes', 'campo']
     template_name = 'cadastros/form.html'
     success_url = reverse_lazy('index') 
+
+###### UPDATE
+class CampoUpdate(UpdateView):
+    model = Campo
+    fields = ['nome','descricao']
+    template_name =  'cadastros/form.html'
+    success_url = reverse_lazy('index')
+
+class AtividadeUpdate(UpdateView):
+    model = Atividade
+    fields = ['numero', 'descricao', 'pontos', 'detalhes', 'campo']
+    template_name = 'cadastros/form.html'
+    success_url = reverse_lazy('index') 
+
+###### DELETE
+class CampoDelete(DeleteView):
+    model = Campo
+    template_name =  'cadastros/form-excluir.html'
+    success_url = reverse_lazy('index')
+
+class AtividadeDelete(DeleteView):
+    model = Atividade
+    template_name =  'cadastros/form-excluir.html'
+    success_url = reverse_lazy('index')
+
